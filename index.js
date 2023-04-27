9cc55777da180889c26020dafd6ad6b13d5111de
sGvQs <susum.earth@gmail.com>
2023-04-27 14:04:23 +0900

    本番用に変更

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? process.env.VERCEL_URL
-        : process.env.DEV_URL,
+    origin: process.env.VERCEL_URL,
   },
 });
 
